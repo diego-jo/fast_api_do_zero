aa6ababfd5a1180a5ea50dd367c53a9eb0d47031
diego <oliveiraj.diego@gmail.com>
2025-03-27 13:51:17 -0300

    aula01-02 finalizada

--- aula01-02/fast_zero/fast_zero/app.py
+++ aula01-02/fast_zero/fast_zero/app.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get(path='/', status_code=200, response_model=Message)
 def read_root():
     return {'message': 'aula01-02'}
+
+
+@app.get('/html', response_class=HTMLResponse)
+def read_html():
+    return """
+    <body>
+        Teste de api com html!
+    <body/>
+    """
--- aula01-02/fast_zero/tests/test_app.py
+++ aula01-02/fast_zero/tests/test_app.py
@@ -15,3 +15,12 @@ def test_root_deve_retornar_ok_e_body_correto():
     # assert
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'aula01-02'}
+
+
+def test_html_deve_retornar_conteudo_esperado():
+    client = TestClient(app)
+
+    response = client.get('/html')
+
+    assert '<body>' in response.text
+    assert 'Teste de api com html!' in response.text
