2dd77b98864e93ee2e86ce5350fd5b56a0155378
diego <oliveiraj.diego@gmail.com>
2025-07-07 08:53:33 -0300

    fix: fix database string connection

--- aula01-11/fast_zero/fast_zero/database/config.py
+++ aula01-11/fast_zero/fast_zero/database/config.py
@@ -1,8 +1,8 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
-from settings import Settings
+from settings import settings
 
-engine = create_async_engine(Settings().DATABASE_URL)
+engine = create_async_engine(settings.DATABASE_URL)
 
 
 async def get_session():
